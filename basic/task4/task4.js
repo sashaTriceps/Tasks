c97dc19153c4c5463df4e489298d5d07650c1121
sashaTriceps <cryzhny@gmail.com>
2017-06-16 15:39:30 +0300

    commit4

---
+++ basic/task4/task4.js
@@ -0,0 +1,11 @@
+function getArea() {
+  var a = 5;
+  var b = 6;
+  var c = 7;
+  var p = (a + b + c)/2;
+  var area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+  console.log('The area of tringle is: ' + area);
+}
+
+getArea();
