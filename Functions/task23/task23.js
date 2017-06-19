80c5f48c9684d8897906366d46368a0fde71c42f
sashaTriceps <cryzhny@gmail.com>
2017-06-19 09:55:26 +0300

    commit23

---
+++ Functions/task23/task23.js
@@ -0,0 +1,22 @@
+function findFirstNotRepeatedChar(string) {
+  var arra1 = string.split('');
+  var result = '';
+  var ctr = 0;
+
+  for (var x = 0; x < arra1.length; x++) {
+    ctr = 0;
+    for (var y = 0; y < arra1.length; y++)
+    {
+      if (arra1[x] === arra1[y]) {
+        ctr+= 1;
+      }
+    }
+    if (ctr < 2) {
+      result = arra1[x];
+    }
+  }
+
+  console.log(result);
+}
+
+findFirstNotRepeatedChar('abacddbec');
