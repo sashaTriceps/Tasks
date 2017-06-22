2916c621755de619f70ab4899ea98cc933f7f6f0
sashaTriceps <cryzhny@gmail.com>
2017-06-22 14:20:25 +0300

    commit8

--- object/task8/task8.js
+++ object/task8/task8.js
@@ -1,37 +1,10 @@
 function Clock() {
-  this.date = new Date();
-  this.hours = this.date.getHours();
-  this.minutes = this.date.getMinutes();
-  this.seconds = this.date.getSeconds();
+  this.time = setInterval(function() {
+    this.date = new Date();
+    console.log(this.date.getHours() + " : " + this.date.getMinutes() + " : " + this.date.getSeconds());
+  }, 1000);
 }
-
-Clock.prototype.run = function() {
-  console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
-};
-
-Clock.prototype.update = function() {
-  this.updateTime(1);
-  setInterval(this.run.bind(this), 1000);
-};
-
-Clock.prototype.updateTime = function(sec) {
-  this.seconds += sec;
-
-  if (this.seconds >= 60) {
-    this.minutes++;
-    this.seconds = 0;
-  }
-
-  if (this.minutes >=60) {
-    this.hours++;
-    this.minutes = 0;
-  }
-
-  if (this.hours >= 24) {
-    this.hours = 0;
-  }
-};
 
 var clock = new Clock();
 
-clock.update();
+clock.time();
