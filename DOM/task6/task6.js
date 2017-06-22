cb0dc6da54f09cbea19c375de7634a481160c2c2
sashaTriceps <cryzhny@gmail.com>
2017-06-22 10:49:12 +0300

    commit6

--- DOM/task6/task6.js
+++ DOM/task6/task6.js
@@ -1,7 +1,14 @@
 function changeContent() {
-  tr = window.prompt('Выберите строку таблицы (0, 1, 2)');
-  td = window.prompt('Виберите ячейку строки (0, 1)');
+  tr = parseInt(window.prompt('Выберите строку таблицы (1, 2, 3)'), 10);
+  td = parseInt(window.prompt('Виберите ячейку строки (1, 2)'), 10);
   text = window.prompt('Введите текст');
-  var table = document.querySelector('#myTable').rows[parseInt(tr, 10)].cells;
-  table[parseInt(td, 10)].innerHTML=text;
+
+  var table = document.querySelector('#myTable');
+
+  var row = table.rows[tr - 1];
+
+  var cells = row.getElementsByTagName('td');
+  var cell = cells[td - 1];
+
+  cell.innerHTML = text;
 }
