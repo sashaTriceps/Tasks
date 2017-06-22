0b42edc0978ffb44cf26f01da081982729b3dff2
sashaTriceps <cryzhny@gmail.com>
2017-06-22 10:07:31 +0300

    commit4

---
+++ DOM/task4/task4.js
@@ -0,0 +1,9 @@
+function getAttributes() {
+  var elem = document.querySelector('#w3r');
+
+  console.log('href : ' + elem.href);
+  console.log('hreflang : ' + elem.hreflang);
+  console.log('rel : ' + elem.rel);
+  console.log('target : ' + elem.target);
+  console.log('type : ' + elem.type);
+}
