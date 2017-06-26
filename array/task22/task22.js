1c2a69819f71b6ebf5000b845eb108f85e7301b1
sashaTriceps <cryzhny@gmail.com>
2017-06-26 09:46:24 +0300

    commit22

---
+++ array/task22/task22.js
@@ -0,0 +1,31 @@
+function union(arr1, arr2) {
+  var result = [];
+
+  if (arr1.length < arr2.length) {
+    for (var i = 0; i < arr2.length; i++) {
+      result.push(arr2[i]);
+    }
+    for (var x = 0; x < arr1.length; x++) {
+      if (result.indexOf(arr1[x]) == -1) {
+        result.push(arr1[x]);
+      }
+    }
+  } else {
+    for (var j = 0; j < arr1.length; j++) {
+      result.push(arr1[j]);
+    }
+    for (var y = 0; y < arr2.length; y++) {
+      if (result.indexOf(arr2[y]) == -1) {
+        result.push(arr2[y]);
+      }
+    }
+  }
+
+  result.sort(function(a, b) {
+    return a - b;
+  });
+
+  console.log(result);
+}
+
+union([100, 2, 1, 10], [1, 2, 3]);
