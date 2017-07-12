dca171923bdf5f27227d6d85ade177142768f86a
sashaTriceps <cryzhny@gmail.com>
2017-07-12 14:55:57 +0300

    добавил проверок, теперь работает

--- array/task4/task4.js
+++ array/task4/task4.js
@@ -1,11 +1,26 @@
-function getLastElem(arr, n) {
+function last(arr, n) {
   var arr1 = [];
   var s = arr.length - n;
 
-  for (var i = s; i < arr.length; i++) {
-    arr1.push(arr[i]);
+  if (n == undefined) {
+    return arr[arr.length - 1];
   }
-  console.log(arr1);
+
+  if (n > arr.length) {
+    for (var i = 0; i < arr.length; i++) {
+      arr1.push(arr[i]);
+    }
+  }
+
+  if (n <= arr.length) {
+    for (var i = s; i < arr.length; i++) {
+      arr1.push(arr[i]);
+    }
+  }
+  
+  return arr1;
 }
 
-getLastElem([1, 2, 3, 4, 5, 6], 3);
+console.log(last([7, 9, 0, -2])); 
+console.log(last([7, 9, 0, -2],3)); 
+console.log(last([7, 9, 0, -2],6));
